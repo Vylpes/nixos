f2ef0a4ce41828e297925f1b2d3cb1cc41bb42b3
Ethan Lane <ethan@vylpes.com>
2024-07-07 21:29:10 +0100

    Setup powerlevel10k

--- system/shared.nix
+++ system/shared.nix
@@ -76,6 +76,7 @@
 
   programs.zsh = {
     enable = true;
+    programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 
     shellAliases = {
         lss = "ls -lah";
