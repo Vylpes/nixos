bc8b0d5adc4b9b12b6068f871cfbc873f650a4ca
Ethan Lane <ethan@vylpes.com>
2024-09-21 14:09:16 +0100

    Install vimplugin

--- config/packages/neovim/default.nix
+++ config/packages/neovim/default.nix
@@ -1,4 +1,11 @@
 {
     home.file."/home/vylpes/.config/nvim/init.lua".source = ./init.lua;
     home.file."/home/vylpes/.config/nvim/coc-settings.json".source = ./coc-settings.json;
+
+    programs.neovim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [
+        packer-nvim
+      ];
+    };
 }
