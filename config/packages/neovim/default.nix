9f8fce9599ca5fc0a6abc614c3d75729f1e7cf0e
Ethan Lane <ethan@vylpes.com>
2024-10-30 12:16:49 +0000

    Remove plugins.lua

--- config/packages/neovim/default.nix
+++ config/packages/neovim/default.nix
@@ -1,5 +1,4 @@
 {
     home.file."/home/vylpes/.config/nvim/init.lua".source = ./init.lua;
     home.file."/home/vylpes/.config/nvim/coc-settings.json".source = ./coc-settings.json;
-    home.file."/home/vylpes/.config/nvim/lua/plugins.lua".source = ./lua/plugins.lua;
 }
