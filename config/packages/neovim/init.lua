baf6c92c30560162ebe0ac82c24c9d66c07498fe
Ethan Lane <ethan@vylpes.com>
2024-10-30 12:05:03 +0000

    Add plugins.lua

--- config/packages/neovim/init.lua
+++ config/packages/neovim/init.lua
@@ -1,3 +1,5 @@
+lua require("plugins")
+
 -- Leader key
 vim.g.mapleader = " "
 
@@ -49,32 +51,6 @@ vim.opt.ttyfast = true
 
 -- Do not add a newline at the end of the file
 vim.opt.fixeol = false
-
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-
-    -- Plugins
-    use 'dracula/vim'
-    use 'ryanoasis/vim-devicons'
-    -- use 'SirVer/ultisnips'
-    use 'honza/vim-snippets'
-    use 'scrooloose/nerdtree'
-    use 'preservim/nerdcommenter'
-    use 'mhinz/vim-startify'
-    use {'neoclide/coc.nvim', branch = 'release'}
-    use 'nvim-lua/plenary.nvim'
-    use 'ThePrimeagen/harpoon'
-    use {'junegunn/fzf', run = function() vim.fn['fzf#install']() end}
-    use 'junegunn/fzf.vim'
-    use 'neovim/nvim-lspconfig'
-    use 'jose-elias-alvarez/null-ls.nvim'
-    use 'MunifTanjim/prettier.nvim'
-    use 'github/copilot.vim'
-    use {'CopilotC-Nvim/CopilotChat.nvim', branch = 'canary'}
-end)
 
 -- Copilot Chat
 local prompts = require('CopilotChat.prompts')
